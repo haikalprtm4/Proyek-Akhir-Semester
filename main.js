dda91b36a730fff0aab1a674f8ec1b5ff69261a0
Haikal Pratama <muhamadhaikalpratamaputra@gmail.com>
2024-11-11 11:41:31 +0700

    1(page)

---
+++ main.js
@@ -0,0 +1,6 @@
+/* const angka = [1,2,3,4,'*',6,7,8]
+const nilai = angka.indexOf('*')
+const hasil = angka[1] - angka[0]
+const jumlah = angka[nilai-1] + hasil
+console.log("Hasilnya adalah",jumlah)
+ | GAK PENTING HAPUSEN AE IKI | */
